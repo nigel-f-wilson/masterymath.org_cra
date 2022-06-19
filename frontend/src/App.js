32964d159a9aa415cf4b1b33c9495b5c43b47141
nolastemgarden <nolastemgarden@gmail.com>
2022-06-18 22:19:21 -0500

    Add Context and theme, and Formspree providers

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,47 +1,31 @@
 import React from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { CssBaseline } from '@mui/material';
+import { FormspreeProvider } from '@formspree/react';
 
-import logo from './logo.svg';
+import Router from './Router'
+// import Navbar from './components/navigation/Navbar';
+
+// THEME & Layout Context
+import theme from "./theme"
+import { ThemeProvider } from '@mui/material/styles';
+import { SettingsContextProvider } from './SettingsContext';
+
+
+
 
 export default function App() {
   return (
-    <div className="App">
-      <h1>Welcome to React Router!</h1>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="about" element={<About />} />
-      </Routes>
-    </div>
+    <React.Fragment>
+      <CssBaseline />
+      <ThemeProvider theme={theme}>
+        <SettingsContextProvider >
+          <FormspreeProvider project="1960964562039602920" >
+          {/* <FormspreeProvider project={process.env.REACT_APP_FORMSPREE_PROJECT_ID} > */}
+            <Router />
+          </FormspreeProvider>
+        </SettingsContextProvider>
+      </ThemeProvider>
+    </React.Fragment>
   );
 }
 
-function Home() {
-  return (
-    <>
-      <main>
-        <h2>Welcome to the homepage!</h2>
-        <p>You can do this, I believe in you.</p>
-      </main>
-      <nav>
-        <Link to="/about">About</Link>
-      </nav>
-    </>
-  );
-}
-
-function About() {
-  return (
-    <>
-      <main>
-        <h2>Who are we?</h2>
-        <p>
-          That feels like an existential question, don't you
-          think?
-        </p>
-      </main>
-      <nav>
-        <Link to="/">Home</Link>
-      </nav>
-    </>
-  );
-}
