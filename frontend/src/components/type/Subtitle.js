fb70269da9ddf4bfe34e3525591bea059cf6de6c
nolastemgarden <nolastemgarden@gmail.com>
2022-06-20 14:37:43 -0500

    Create Subtitle.js

---
+++ frontend/src/components/type/Subtitle.js
@@ -0,0 +1,48 @@
+import React, { useContext } from 'react'
+import { Typography } from '@mui/material';
+
+import { SettingsContext } from "../../SettingsContext";
+
+export default function Subtitle(props) {
+  const { text, 
+    gutterBottom = false, 
+    fontFamily= 'roboto', 
+    align = 'center' 
+  } = props
+  
+  const { colorTheme } = useContext(SettingsContext)
+
+  const padding = (gutterBottom) ? "0 0 3rem" : "0"
+
+  const textColor = (colorTheme === "dark") ? "white" : "black"
+
+
+  return (
+    <Typography 
+      color={textColor}
+      align={align}
+      fontFamily={fontFamily}
+      sx={{
+        padding: padding, 
+        fontWeight: 700,
+        letterSpacing: '2px',
+        fontSize: '1.8rem',
+        lineHeight: '3.0rem',
+        // '@media (min-width: 450px)': {
+        //   fontSize: '3.8rem',
+        //   lineHeight: '4.3rem',
+        // },
+        // '@media (min-width: 600px)': {
+        //   fontSize: '4.2rem',
+        //   lineHeight: '4.7rem',
+        // },
+        // '@media (min-width: 900px)': {
+        //   fontSize: '5.0rem',
+        //   lineHeight: '6.0rem', 
+        // },
+      }}
+    >
+      { text }
+    </Typography>
+  )
+}
