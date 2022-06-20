3f94aa85571f281c2a92b0f4beb2780cf2f0c079
nolastemgarden <nolastemgarden@gmail.com>
2022-06-20 14:37:45 -0500

    Create Title.js

---
+++ frontend/src/components/type/Title.js
@@ -0,0 +1,44 @@
+import React, { useContext } from 'react'
+import { Typography } from '@mui/material';
+
+import { SettingsContext } from "../../SettingsContext";
+
+export default function Title(props) {
+  const { text, gutterBottom, fontFamily } = props
+  
+  const { colorTheme } = useContext(SettingsContext)
+
+  const padding = (gutterBottom) ? "0 0 2rem" : "0"
+
+  const textColor = (colorTheme === "dark") ? "white" : "black"
+
+
+  return (
+    <Typography 
+      color={textColor}
+      align="center" 
+      fontFamily={fontFamily}
+      sx={{
+        padding: padding, 
+        fontWeight: 700,
+        letterSpacing: '5px',
+        fontSize: '6.0rem',
+        lineHeight: '6.5rem',
+        // '@media (min-width: 450px)': {
+        //   fontSize: '3.8rem',
+        //   lineHeight: '4.3rem',
+        // },
+        // '@media (min-width: 600px)': {
+        //   fontSize: '4.2rem',
+        //   lineHeight: '4.7rem',
+        // },
+        // '@media (min-width: 900px)': {
+        //   fontSize: '5.0rem',
+        //   lineHeight: '6.0rem', 
+        // },
+      }}
+    >
+      { text }
+    </Typography>
+  )
+}
