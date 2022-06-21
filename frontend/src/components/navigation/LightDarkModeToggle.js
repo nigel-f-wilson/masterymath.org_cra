31a093c77410e1b1307a363614a0510e7a792933
nolastemgarden <nolastemgarden@gmail.com>
2022-06-21 17:46:51 -0500

    Create LightDarkModeToggle.js
    Copied from this guide: https://www.youtube.com/watch?v=ad9f-EYtWPo&ab_channel=WebDevEducation

---
+++ frontend/src/components/navigation/LightDarkModeToggle.js
@@ -0,0 +1,26 @@
+import React, { useContext } from "react";
+import "./lightDarkModeToggle.css";
+
+import { SettingsContext } from '../../SettingsContext';
+
+
+export default function LightDarkModeToggle(props) {
+  const { colorTheme, toggleColorTheme } = useContext(SettingsContext)
+
+  let toggled = (colorTheme === "dark") ? true : false
+
+  return (
+      <div onClick={toggleColorTheme} className={`toggle${toggled ? " night" : ""}`}>
+          <div className="notch">
+              <div className="crater" />
+              <div className="crater" />
+          </div>
+          <div>
+              <div className="shape sm" />
+              <div className="shape sm" />
+              <div className="shape md" />
+              <div className="shape lg" />
+          </div>
+      </div>
+  );
+}
