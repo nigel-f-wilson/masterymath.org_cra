3764f4155c42303d3ee4c38f486694aea135e24a
nolastemgarden <nolastemgarden@gmail.com>
2022-06-21 17:48:13 -0500

    Add comment link to source

--- frontend/src/components/navigation/LightDarkModeToggle.js
+++ frontend/src/components/navigation/LightDarkModeToggle.js
@@ -1,12 +1,11 @@
+// Source --> https://www.youtube.com/watch?v=ad9f-EYtWPo&ab_channel=WebDevEducation
 import React, { useContext } from "react";
 import "./lightDarkModeToggle.css";
 
 import { SettingsContext } from '../../SettingsContext';
 
-
 export default function LightDarkModeToggle(props) {
   const { colorTheme, toggleColorTheme } = useContext(SettingsContext)
-
   let toggled = (colorTheme === "dark") ? true : false
 
   return (
