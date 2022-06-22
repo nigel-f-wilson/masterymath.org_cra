a7c4aa5c8835cbaae48f723ebb6dd125fd1aa7b9
nolastemgarden <nolastemgarden@gmail.com>
2022-06-22 09:49:58 -0500

    Update WelcomePage.js

--- frontend/src/pages/WelcomePage.js
+++ frontend/src/pages/WelcomePage.js
@@ -3,7 +3,7 @@ import React from 'react'
 // THIRD PARTY
 import { Link as RouterLink } from 'react-router-dom';
 import { HashLink, NavHashLink } from 'react-router-hash-link';
-import { Box, Grid, Container, Card, CardHeader, CardMedia, Button } from '@mui/material';
+import { Box, Grid, Container, Card, CardHeader, CardMedia, Button, Typography } from '@mui/material';
 
 
 // ICONS
@@ -12,7 +12,7 @@ import { faArrowCircleDown } from '@fortawesome/free-solid-svg-icons'
 // import { faCircleChevronDown } from '@fortawesome/free-solid-svg-icons'
 
 // CUSTOM
-import { Title, Subtitle } from "../components/type";
+import { Title, Subtitle, ButtonLabel } from "../components/type";
 import { Navbar } from "../components/navigation";
 
 
@@ -21,9 +21,10 @@ export default function WelcomePage() {
     <Box>
       <Navbar />
       <TitleSection id='title-section' />
-      <LandingSection id='section-one' />
-      <LandingSection id='section-two' />
-      <LandingSection id='section-three' />
+      <LandingSection id='what' />
+      <LandingSection id='who' />
+      <LandingSection id='how' />
+      <LandingSection id='why' />
     </Box>
   )
 }
@@ -32,32 +33,9 @@ function TitleSection(props) {
   const { id } = props
   return (
     <Box id="title-section" sx={{ backgroundColor: '#000',  height: '100vh' }}  >
-        <Header />
-        <ScrollLinks />
-        {/* <h2>Section Three</h2>
-        <h3>Go to:</h3>
-        <ul>
-          <li>
-            <HashLink to="/">Home</HashLink>
-          </li>
-          <li>
-            <HashLink smooth to={'/#section-one'}>
-              Section One
-            </HashLink>
-          </li>
-          <li>
-            <HashLink smooth to={'/#section-two'}>
-              Section Two
-            </HashLink>
-          </li>
-          
-          <li>
-            <HashLink smooth to="#top">
-              Top of Page
-            </HashLink>
-          </li>
-        </ul> */}
-      </Box>
+      <Header />
+      <ScrollLinks />
+    </Box>
   )
 }
 
@@ -65,6 +43,7 @@ function LandingSection(props) {
   const { id } = props
   return (
     <Box sx={{ backgroundColor: '#D0D0D0', padding: '2.5vw 3.5vw 12vh 3.5vw',  height: '100vh' }} id="section-three">
+        <Title text={id} />
         <h2>Section Three</h2>
         <h3>Go to:</h3>
         <ul>
@@ -103,19 +82,25 @@ function Header() {
       <Subtitle text="in mental computation." fontFamily="Amatic SC"  />
     </Box>
   )
-
 }
 
 function ScrollLinks(props) {
   // const {  } = props
+  
+  
   return (
     <Box >
-      {/* <Button 
+      <Button 
         variant="outlined"
-        color="white"
+        component="HashLink"
+        to={'/#section-one'}
+
+        // sx={{ color: 'white'}}
+        startIcon={<FontAwesomeIcon icon={faArrowCircleDown} size='sm' color="white" />}
+        children={<ButtonLabel text="Who?" fontFamily="roboto" color="white" />}
       >
-        <FontAwesomeIcon icon={faArrowCircleDown} color='white' size='sm' />
-      </Button> */}
+        
+      </Button>
       
 
 
