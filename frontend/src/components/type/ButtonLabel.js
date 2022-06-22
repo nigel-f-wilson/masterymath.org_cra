5ed452e41afc7bb1bf69646c4b1f2e8affe4ca8b
nolastemgarden <nolastemgarden@gmail.com>
2022-06-22 17:16:11 -0500

    Add icons to my ButtonLabel

--- frontend/src/components/type/ButtonLabel.js
+++ frontend/src/components/type/ButtonLabel.js
@@ -1,5 +1,8 @@
 import React, { useContext } from 'react'
-import { Typography } from '@mui/material';
+import { Box, Typography } from '@mui/material';
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
 
 import { SettingsContext } from "../../SettingsContext";
 
@@ -7,7 +10,9 @@ export default function ButtonLabel(props) {
   const { text, 
     gutterBottom = false, 
     fontFamily= 'roboto', 
-    align = 'center' 
+    align = 'center',
+    icon, 
+    iconSize = 'lg'
   } = props
   
   const { colorTheme } = useContext(SettingsContext)
@@ -18,31 +23,34 @@ export default function ButtonLabel(props) {
 
 
   return (
-    <Typography 
-      color={textColor}
-      align={align}
-      fontFamily={fontFamily}
-      sx={{
-        padding: padding, 
-        fontWeight: 700,
-        // letterSpacing: '2px',
-        fontSize: '1.2rem',
-        lineHeight: '2.0rem',
-        // '@media (min-width: 450px)': {
-        //   fontSize: '3.8rem',
-        //   lineHeight: '4.3rem',
-        // },
-        // '@media (min-width: 600px)': {
-        //   fontSize: '4.2rem',
-        //   lineHeight: '4.7rem',
-        // },
-        // '@media (min-width: 900px)': {
-        //   fontSize: '5.0rem',
-        //   lineHeight: '6.0rem', 
-        // },
-      }}
-    >
-      { text }
-    </Typography>
+    <React.Fragment>
+        <FontAwesomeIcon icon={icon} color={textColor} size={iconSize} />
+        <Box pr={1} />
+        <Typography 
+          children={text}
+          color={textColor}
+          align={align}
+          fontFamily={fontFamily}
+          sx={{
+            padding: padding, 
+            fontWeight: 700,
+            // letterSpacing: '2px',
+            fontSize: '1.4rem',
+            lineHeight: '2.0rem',
+            // '@media (min-width: 450px)': {
+            //   fontSize: '3.8rem',
+            //   lineHeight: '4.3rem',
+            // },
+            // '@media (min-width: 600px)': {
+            //   fontSize: '4.2rem',
+            //   lineHeight: '4.7rem',
+            // },
+            // '@media (min-width: 900px)': {
+            //   fontSize: '5.0rem',
+            //   lineHeight: '6.0rem', 
+            // },
+          }}
+        />
+    </React.Fragment>
   )
 }
