90362ebd99bfba5fcfe7aede5ce6f001cffb604a
nolastemgarden <nolastemgarden@gmail.com>
2022-06-23 10:15:33 -0500

    Position the switch

--- frontend/src/components/navigation/LightDarkModeToggle.js
+++ frontend/src/components/navigation/LightDarkModeToggle.js
@@ -2,14 +2,21 @@
 import React, { useContext } from "react";
 import "./lightDarkModeToggle.css";
 
+import { Box } from '@mui/material';
+
 import { SettingsContext } from '../../SettingsContext';
+import { ButtonLabel } from "../type";
 
 export default function LightDarkModeToggle(props) {
   const { colorTheme, toggleColorTheme } = useContext(SettingsContext)
+  
   let toggled = (colorTheme === "dark") ? true : false
+  let label = (colorTheme === "dark") ? "Dark Theme" : "Light Theme"
 
   return (
-      <div onClick={toggleColorTheme} className={`toggle${toggled ? " night" : ""}`}>
+    <Box position='absolute' bottom='1.0rem' width='100%' display='flex' flexDirection='row' justifyContent='start' alignItems='center' >
+      <Box width='50px' >
+        <div onClick={toggleColorTheme} className={`toggle${toggled ? " night" : ""}`}>
           <div className="notch">
               <div className="crater" />
               <div className="crater" />
@@ -20,6 +27,9 @@ export default function LightDarkModeToggle(props) {
               <div className="shape md" />
               <div className="shape lg" />
           </div>
-      </div>
+        </div>
+      </Box>
+      <ButtonLabel text={label} />
+    </Box>
   );
 }
