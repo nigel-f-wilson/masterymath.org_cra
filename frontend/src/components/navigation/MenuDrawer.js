69162b9902ee0fb07003db382fff6773ac037baa
nolastemgarden <nolastemgarden@gmail.com>
2022-06-29 07:06:11 -0500

    Add auth stubs

--- frontend/src/components/navigation/MenuDrawer.js
+++ frontend/src/components/navigation/MenuDrawer.js
@@ -1,7 +1,7 @@
-import React, { useContext, useState } from 'react'
+import React, { useContext } from 'react'
 
 // THIRD PARTY
-import { Link as RouterLink } from 'react-router-dom';
+import { useAuth0 } from "@auth0/auth0-react";
 import { Box, Drawer , IconButton, Typography } from '@mui/material';
 
 // ICONS
@@ -9,18 +9,17 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faTimes } from '@fortawesome/free-solid-svg-icons'
 
 // CUSTOM
-import { SettingsContext } from '../../SettingsContext';
-import { LayoutContext } from '../../LayoutContext';
-import { Title, Subtitle } from "../type";
 import { LightDarkModeToggle } from "./";
+import { LoginButton, LogoutButton, ProfileSummary } from '../auth';
+import { SettingsContext, LayoutContext } from '../../contexts';
 
 
 export default function MenuDrawer(props) {
   const { drawerOpen, toggleDrawer } = props
 
+  const { isAuthenticated } = useAuth0()
   const { colorTheme } = useContext(SettingsContext)
   const { navbarHeightPx } = useContext(LayoutContext)
-
   
   const fontColor = (colorTheme === "light") ? "black" : "white"
   const bgColor = (colorTheme === "light") ? "lightGrey" : "darkGrey"
@@ -33,38 +32,37 @@ export default function MenuDrawer(props) {
       // onClick={toggleDrawer}
       // onKeyDown={toggleDrawer}
     >
-      
       <Box
         width='320px'
-        height='100vh'
-        color={fontColor}
-        bgcolor={bgColor} 
+        height='100vh' 
         paddingX='1rem'
-        // display='flex'
-        // flexDirection='column'
+        color={fontColor}
+        bgcolor={bgColor}
       >
-
         <Box display='flex' justifyContent='end' height={`${navbarHeightPx}px`}   >
           <IconButton 
             onClick={toggleDrawer}
-            children={<FontAwesomeIcon icon={faTimes} color={fontColor} />}
+            children={<FontAwesomeIcon icon={faTimes} color={fontColor} size='lg' />}
             aria-controls="main-menu"
             aria-haspopup="true"
             sx={{ padding: 0 }}
           />
         </Box>
 
-        <Box width='100%' border='solid red 1px' height='8rem' >
-          PROFILE AREA
-          You are signed in as: 
+        <ProfileSummary />
           
-        </Box>
 
         <Typography variant='h3' children='About' />
 
         <Typography variant='h3' children='Services' />
         
         <Typography variant='h3' children='Resources' />
+
+        <Box width='100%' border='solid red 1px' height='8rem' >
+          
+          {!isAuthenticated && ( <LoginButton /> )}
+          {isAuthenticated && ( <LogoutButton /> )}
+        </Box>
         
         <LightDarkModeToggle />
       
