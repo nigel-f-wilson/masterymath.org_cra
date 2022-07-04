75b675d38a92216d4be9fe8eb6496aec29298326
nolastemgarden <nolastemgarden@gmail.com>
2022-07-04 16:01:47 -0500

    Add padding on both sides of button label text

--- frontend/src/components/text/ButtonLabel.js
+++ frontend/src/components/text/ButtonLabel.js
@@ -25,46 +25,47 @@ export default function ButtonLabel(props) {
 
   return (
     <React.Fragment>
-        {
-          startIcon ? (
-            <FontAwesomeIcon icon={startIcon} color={textColor} size={iconSize} />
-          ) : (
-            <></>
-          )
-        }
-        <Box pr={1} />
-        <Typography 
-          children={text}
-          color={textColor}
-          align={align}
-          fontFamily={fontFamily}
-          sx={{
-            padding: padding, 
-            fontWeight: 700,
-            // letterSpacing: '2px',
-            fontSize: '1.4rem',
-            lineHeight: '2.0rem',
-            // '@media (min-width: 450px)': {
-            //   fontSize: '3.8rem',
-            //   lineHeight: '4.3rem',
-            // },
-            // '@media (min-width: 600px)': {
-            //   fontSize: '4.2rem',
-            //   lineHeight: '4.7rem',
-            // },
-            // '@media (min-width: 900px)': {
-            //   fontSize: '5.0rem',
-            //   lineHeight: '6.0rem', 
-            // },
-          }}
-        />
-        {
-          endIcon ? (
-            <FontAwesomeIcon icon={endIcon} color={textColor} size={iconSize} />
-          ) : (
-            <></>
-          )
-        }
+      {
+        startIcon ? (
+          <FontAwesomeIcon icon={startIcon} color={textColor} size={iconSize} />
+        ) : (
+          <></>
+        )
+      }
+        <Box px={1} >
+          <Typography 
+            children={text}
+            color={textColor}
+            align={align}
+            fontFamily={fontFamily}
+            sx={{
+              padding: padding, 
+              fontWeight: 700,
+              // letterSpacing: '2px',
+              fontSize: '1.4rem',
+              lineHeight: '2.0rem',
+              // '@media (min-width: 450px)': {
+              //   fontSize: '3.8rem',
+              //   lineHeight: '4.3rem',
+              // },
+              // '@media (min-width: 600px)': {
+              //   fontSize: '4.2rem',
+              //   lineHeight: '4.7rem',
+              // },
+              // '@media (min-width: 900px)': {
+              //   fontSize: '5.0rem',
+              //   lineHeight: '6.0rem', 
+              // },
+            }}
+          />
+        </Box>
+      {
+        endIcon ? (
+          <FontAwesomeIcon icon={endIcon} color={textColor} size={iconSize} />
+        ) : (
+          <></>
+        )
+      }
     </React.Fragment>
   )
 }
