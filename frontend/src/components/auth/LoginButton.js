90dce0593137269f9db95426ac47b3f7f03f4916
nolastemgarden <nolastemgarden@gmail.com>
2022-07-04 15:58:57 -0500

    Style Login and Logout Buttons

--- frontend/src/components/auth/LoginButton.js
+++ frontend/src/components/auth/LoginButton.js
@@ -1,16 +1,34 @@
-import React from "react";
+import React, { useContext } from "react";
+
+// THIRD PARTY
+import { Box } from '@mui/material';
 import { useAuth0 } from "@auth0/auth0-react";
 
-const LoginButton = () => {
+// ICONS
+import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons'
+
+// CUSTOM
+import { SettingsContext } from "../../contexts";
+import { ButtonLabel } from "../text";
+
+export default function LoginButton() {
   const { loginWithRedirect } = useAuth0();
+
+  const { colorTheme } = useContext(SettingsContext)
+  const fontColor = (colorTheme === "dark") ? "white" : "black"
+
   return (
-    <button
-      className="btn btn-primary btn-block"
+    <Box 
       onClick={() => loginWithRedirect()}
+      border={`solid ${fontColor} 1px`} 
+      borderRadius={4} 
+      height='2.7rem' 
+      width='100%' 
+      display='flex' 
+      justifyContent='center' 
+      alignItems='center' 
     >
-      Log In
-    </button>
+      <ButtonLabel text="Log In" endIcon={faArrowCircleRight} />
+    </Box>
   );
 };
-
-export default LoginButton;
--- frontend/src/components/auth/LogoutButton.js
+++ frontend/src/components/auth/LogoutButton.js
@@ -1,20 +1,37 @@
-import React from "react";
+import React, { useContext } from "react";
+
+// THIRD PARTY
+import { Box } from '@mui/material';
 import { useAuth0 } from "@auth0/auth0-react";
 
-const LogoutButton = () => {
+// ICONS
+// import { fasArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+import LogoutIcon from '@mui/icons-material/Logout';
+
+// CUSTOM
+import { SettingsContext } from "../../contexts";
+import { ButtonLabel } from "../text";
+
+export default function LogoutButton() {
   const { logout } = useAuth0();
+
+  const { colorTheme } = useContext(SettingsContext)
+  const fontColor = (colorTheme === "dark") ? "white" : "black"
+
   return (
-    <button
-      className="btn btn-danger btn-block"
-      onClick={() =>
-        logout({
-          returnTo: window.location.origin,
-        })
-      }
+    <Box 
+      onClick={() => logout()}
+      border={`solid ${fontColor} 1px`} 
+      borderRadius={4} 
+      height='2.7rem' 
+      width='100%' 
+      display='flex' 
+      justifyContent='center' 
+      alignItems='center' 
     >
-      Log Out
-    </button>
+      <ButtonLabel text="Log Out"  />
+      <Box pr={1} />
+      <LogoutIcon />
+    </Box>
   );
 };
-
-export default LogoutButton;
