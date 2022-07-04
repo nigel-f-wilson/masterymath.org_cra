d2fcaa42fb545ddbe4b470cf8f0fe153fd432066
nolastemgarden <nolastemgarden@gmail.com>
2022-07-04 18:13:05 -0500

    Remove handling for not authenticated
    This responsibility belongs to the parent that wants to render this component now.

--- frontend/src/components/auth/ProfileSummary.js
+++ frontend/src/components/auth/ProfileSummary.js
@@ -1,33 +1,33 @@
 import React, { useContext } from "react";
 
+// THIRD PARTY
 import { useAuth0 } from "@auth0/auth0-react";
-import { Box, Stack, Avatar, Typography } from '@mui/material';
+import { Stack, Avatar, Typography } from '@mui/material';
+
+// CUSTOM
+import { FlexRow } from "../layout";
 
 import { SettingsContext } from "../../contexts";
 
 export default function ProfileSummary(props) {
-  const { user, isAuthenticated } = useAuth0();
-  // const { name, email, picture } = user
+  // const { user, isAuthenticated } = useAuth0();
+  const { name, email, picture } = props.user
 
   const { colorTheme } = useContext(SettingsContext)
 
-  console.log(`USER: ${JSON.stringify(user,null,4)}`);
+  console.log(`USER: ${JSON.stringify(props.user,null,4)}`);
 
   return (
-    <Box width="100%" height='80px' display='flex' flexDirection='row' alignItems='center' mb={2} >
-      {isAuthenticated && user && (
-        <>
-          <Avatar
-            src={user.picture}
-            alt={user.name}
-            sx={{ width: 60, height: 60, marginX: '15px' }}
-          />
-          <Stack  display='flex' flexDirection='column'  justifyContent='center' py='10px' >
-            <Typography variant='h4' children={user.name} />
-            <Typography variant='body1' children={user.email} />
-          </Stack>
-        </>
-      )}
-    </Box>
+    <>
+      <Avatar
+        src={picture}
+        alt={name}
+        sx={{ width: 60, height: 60, marginX: '15px' }}
+      />
+      <Stack  display='flex' flexDirection='column'  justifyContent='center' py='10px' >
+        <Typography variant='h4' children={name} />
+        <Typography variant='body1' children={email} />
+      </Stack>
+    </>
   );
 };
