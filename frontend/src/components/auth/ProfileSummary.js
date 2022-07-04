9c48700ff79bc7aae53481389df3986b53ef4dcc
nolastemgarden <nolastemgarden@gmail.com>
2022-07-04 15:59:59 -0500

    Remove login button from profile summary
    Either login or logout is rendered right below the summary so this code was creating a duplicate log in button

--- frontend/src/components/auth/ProfileSummary.js
+++ frontend/src/components/auth/ProfileSummary.js
@@ -4,23 +4,17 @@ import { useAuth0 } from "@auth0/auth0-react";
 import { Box, Stack, Avatar, Typography } from '@mui/material';
 
 import { SettingsContext } from "../../contexts";
-import { LoginButton } from "../auth"
 
 export default function ProfileSummary(props) {
   const { user, isAuthenticated } = useAuth0();
   // const { name, email, picture } = user
 
   const { colorTheme } = useContext(SettingsContext)
-  const fontColor = (colorTheme === "dark") ? "white" : "black"
-
 
   console.log(`USER: ${JSON.stringify(user,null,4)}`);
 
   return (
-    <Box width="100%" height='80px' display='flex' flexDirection='row' alignItems='center' mb={2} border={`solid ${fontColor} 1px`} >
-      {!isAuthenticated && (
-        <LoginButton />
-      )}
+    <Box width="100%" height='80px' display='flex' flexDirection='row' alignItems='center' mb={2} >
       {isAuthenticated && user && (
         <>
           <Avatar
@@ -34,9 +28,6 @@ export default function ProfileSummary(props) {
           </Stack>
         </>
       )}
-      
-      
-
     </Box>
   );
 };
