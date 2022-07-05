08d3cc98a5085eb694a22bb1ef04d17c239d1bf2
nolastemgarden <nolastemgarden@gmail.com>
2022-07-05 08:59:20 -0500

    Simplify styles using ButtonWrapper

--- frontend/src/components/buttons/LoginButton.js
+++ frontend/src/components/buttons/LoginButton.js
@@ -1,34 +1,23 @@
-import React, { useContext } from "react";
+import React from "react";
 
 // THIRD PARTY
-import { Box } from '@mui/material';
 import { useAuth0 } from "@auth0/auth0-react";
-
-// ICONS
 import { faArrowCircleRight } from '@fortawesome/free-solid-svg-icons'
 
-// CUSTOM
-import { SettingsContext } from "../../contexts";
-import { ButtonLabel } from "../text";
+// INTERNAL
+import { ButtonWrapper, ButtonLabel } from "./";
 
 export default function LoginButton() {
   const { loginWithRedirect } = useAuth0();
-
-  const { colorTheme } = useContext(SettingsContext)
-  const fontColor = (colorTheme === "dark") ? "white" : "black"
-
   return (
-    <Box 
+    <ButtonWrapper 
+      id="login-button-wrapper"
       onClick={() => loginWithRedirect()}
-      border={`solid ${fontColor} 1px`} 
-      borderRadius={4} 
-      height='2.7rem' 
-      width='100%' 
-      display='flex' 
-      justifyContent='center' 
-      alignItems='center' 
     >
-      <ButtonLabel text="Log In" endIcon={faArrowCircleRight} />
-    </Box>
+      <ButtonLabel 
+        text="Log In" 
+        endIcon={faArrowCircleRight} 
+      />
+    </ButtonWrapper>
   );
 };
--- frontend/src/components/buttons/LogoutButton.js
+++ frontend/src/components/buttons/LogoutButton.js
@@ -1,37 +1,26 @@
-import React, { useContext } from "react";
+import React from "react";
 
 // THIRD PARTY
 import { Box } from '@mui/material';
 import { useAuth0 } from "@auth0/auth0-react";
 
 // ICONS
-// import { fasArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'
+// import { fasArrowRightFromBracket } from '@fortawesome/free-solid-svg-icons'  // NOT FREE ??
 import LogoutIcon from '@mui/icons-material/Logout';
 
-// CUSTOM
-import { SettingsContext } from "../../contexts";
-import { ButtonLabel } from "../text";
+// INTERNAL
+import { ButtonWrapper, ButtonLabel } from "./";
 
 export default function LogoutButton() {
   const { logout } = useAuth0();
-
-  const { colorTheme } = useContext(SettingsContext)
-  const fontColor = (colorTheme === "dark") ? "white" : "black"
-
   return (
-    <Box 
+    <ButtonWrapper 
+      id="logout-button-wrapper"
       onClick={() => logout()}
-      border={`solid ${fontColor} 1px`} 
-      borderRadius={4} 
-      height='2.7rem' 
-      width='100%' 
-      display='flex' 
-      justifyContent='center' 
-      alignItems='center' 
     >
       <ButtonLabel text="Log Out"  />
       <Box pr={1} />
       <LogoutIcon />
-    </Box>
+    </ButtonWrapper>
   );
 };
